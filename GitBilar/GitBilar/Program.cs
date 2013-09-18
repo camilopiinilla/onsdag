d369a4439165691298742a8632b9d3ff4c45a43c
unknown <mikael.shiravand@R9ZBVYA.learnet.se>
2013-09-18 10:10:44 +0200

    jag testar att skapa en ny klass.cs

--- GitBilar/GitBilar/Program.cs
+++ GitBilar/GitBilar/Program.cs
@@ -10,13 +10,17 @@ namespace GitBilar
     {
         static void Main(string[] args)
         {
-            Bil bmw = new Bil();
+            Bil bmw = new Bil("BMW", "118i", 2006);
+
+            Console.WriteLine(bmw.Brand);
+            Console.WriteLine(bmw.Model);
+            Console.WriteLine(bmw.Year);
         }
         public class Bil
         {
             public string Brand { get; set; }
             public string Model { get; set; }
-            public int year { get; set; }
+            public int Year { get; set; }
 
             public Bil(string brand, string model, int year)
             {
